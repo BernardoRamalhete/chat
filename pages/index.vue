fc20c0e61914ae99c4ce5b9ba4e84d99c8f63470
BernardoRamalhete <97164408+BernardoRamalhete@users.noreply.github.com>
2023-08-18 20:05:13 -0300

    :alembic: Experimenting logo setup

--- pages/index.vue
+++ pages/index.vue
@@ -1,60 +1,13 @@
 <template>
     <div>
-        <button @click="signIn">Sign in with google</button>
-        <pre>{{ user }}</pre>
-        <button @click="mockThings">Create docs</button>
-        <pre>{{ result }}</pre>
-        <pre>
-            {{ firestoreData.get('chats').length }}
-        </pre>
+        <AppLogo :spread="0.2" :pallet="['FAD2E1', 'CDDAFD', 'BEE1E6']"/>
     </div>
 </template>
 
 <script setup>
-const { user, signIn } = useFirebaseAuth()
-const { create, subscribe, firestoreData } = useFirebaseDb()
 
-const result = ref('Nothing new yet')
-async function mockThings() {
-
-    const data = {
-        users: [
-            {
-                name: 'User 1',
-                email: 'user1@email.com',
-                picture: 'https://picsum.photos/seed/dog/200'
-            },
-            {
-                name: 'User 2',
-                email: 'user2@email.com',
-                picture: 'https://picsum.photos/seed/cat/200'
-            }
-        ],
-        messages: [
-            {
-                text: 'Hello',
-                author: 'user1@email.com'
-            },
-            {
-                text: 'Hi',
-                author: 'user2@email.com'
-            },
-            {
-                text: 'How u doing?',
-                author: 'user2@email.com'
-            }
-        ] 
-    }
-    result.value = await create('chats', data)
-}
-
-onMounted(() => {
-    subscribe('chats')
-})
 </script>
 
 <style lang="scss" scoped>
-div {
-    border: 1px solid $pure_black;
-}
+
 </style>
