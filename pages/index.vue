dc20cc15d37055af9a019454b819abe40aa377f4
BernardoRamalhete <97164408+BernardoRamalhete@users.noreply.github.com>
2023-08-19 15:34:40 -0300

    :construction: Creating login/home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
 <template>
-    <div>
-        <AppLogo :spread="0.2" :pallet="['FAD2E1', 'CDDAFD', 'BEE1E6']"/>
-    </div>
+    <main>
+        <div>
+            <AppLogo 
+                :spread="0.2" 
+                :pallet="['FAD2E1', 'CDDAFD', 'BEE1E6']"
+            />
+
+            <h1 class="hero-title">
+                <AppMorphingText :text="['Private Messages', 'Group Chats', 'All Encrypted']" :morphDuration="1"/>
+            </h1>
+            <p>
+                Instantly connect with your friends and family with out sharing any information without your consent.
+            </p>
+            <AppButton class="primary">
+                Join now
+            </AppButton>
+        </div>
+        <div>
+
+        </div>
+    </main>
 </template>
 
 <script setup>
@@ -9,5 +27,7 @@
 </script>
 
 <style lang="scss" scoped>
-
+.hero-title {
+    font-size: 80px;
+}
 </style>
