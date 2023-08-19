5f10e85b4750de86945c68a8b3786f1c1bb2cdc7
BernardoRamalhete <97164408+BernardoRamalhete@users.noreply.github.com>
2023-08-19 15:33:35 -0300

    :sparkles: Create component for handling buttons

---
+++ components/AppButton.vue
@@ -0,0 +1,38 @@
+<template>
+    <button class="main-button">
+        <slot/>
+    </button>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+.main-button {
+    padding: 12px 40px;
+    border-radius: 60px;
+    background-color: transparent;
+    color: $title_color;
+    border: none;
+    transition: all 0.4s ease;
+    cursor: pointer;
+    &:focus {
+        outline: transparent;
+    }
+
+    &.primary {
+        background-color: $primary_blue;
+        color: $light_background;
+        font-weight: 600;
+        font-size: 20px;
+        position: relative;
+        background-size: 100% 300%;
+        background-image: $primary_button_gradient;
+        box-shadow: $primary_button_box_shadow;
+        &:hover {
+            background-position: 0 100%;
+        }
+    }
+}
+</style>
