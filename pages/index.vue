57f842b3d23c79807e906ff36f8cd1a6b6435df5
BernardoRamalhete <97164408+BernardoRamalhete@users.noreply.github.com>
2023-08-19 21:39:40 -0300

    :construction: Adding content

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,35 @@
 <template>
-    <main>
-        <div>
+    <main class="wrapper">
+        <div class="content-section">
             <AppLogo 
+                class="logo"
                 :spread="0.2" 
-                :pallet="['FAD2E1', 'CDDAFD', 'BEE1E6']"
+                :pallet="['ff006e', '3a86ff', '8338ec']"
             />
-
-            <h1 class="hero-title">
-                <AppMorphingText :text="['Private Messages', 'Group Chats', 'All Encrypted']" :morphDuration="1"/>
-            </h1>
-            <p>
-                Instantly connect with your friends and family with out sharing any information without your consent.
-            </p>
-            <AppButton class="primary">
-                Join now
-            </AppButton>
+            <div class="hero">
+                <h1 class="hero-title">
+                    <AppMorphingText :text="['Private Chats', 'Group Chats', 'All Encrypted']" :morphDuration="1"/>
+                </h1>
+                <p class="hero-text">
+                    Instantly connect with your friends and family with out sharing any information without your consent.
+                </p>
+                <span class="hero-cta">
+                    <AppButton class="primary ">
+                        Join now
+                    </AppButton>
+                </span>
+            </div>
         </div>
-        <div>
-
+        <div class="image-section">
+            <div class="image-top"></div>
+            <div class="button">
+                <AppButton class="primary">
+                    Login With 
+                    <Icon name="logos:google-icon" aria-hidden/>
+                    <span class="visually-hidden">Google</span>
+                </AppButton>
+            </div>
+            <div class="image-container"></div>
         </div>
     </main>
 </template>
@@ -27,7 +39,85 @@
 </script>
 
 <style lang="scss" scoped>
-.hero-title {
-    font-size: 80px;
+
+.wrapper {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 20px;
+    padding: 60px;
+    height: 100%;
+
+    .content-section {
+        position: relative;
+        padding-top: 120px;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        .logo {
+            position: absolute;
+            top: -20px;
+            left: -64px;
+        }
+        .hero {
+            margin-block: auto;
+            max-width: 90%;
+            &-title {
+                font-size: 72px;
+                white-space: nowrap;
+            }
+            &-text {
+                margin: 0 0 48px 4px;
+                font-size: 20px;
+            }
+            &-cta {
+                display: flex;
+                justify-content: flex-end;
+            }
+        }
+    }
+
+    .image-section {
+        display: grid;
+        grid-template-columns: 1fr 300px;
+        grid-template-rows: max-content auto;
+        .image {
+            &-top {
+                background-color: $light_pink;
+                width: 100%;
+                height: 80%;
+                top: 20%;
+                border-radius: 20px 20px 0 0;
+                position: relative;
+                &:after {
+                    content: '';
+                    background-color: $light_pink;
+                    background-color: red;
+                    width: 40px;
+                    aspect-ratio: 1;
+                    position: absolute;
+                    left: 100%;
+                    bottom: 0;
+                    background-image: radial-gradient(circle at 125% -25%, 
+                        $light_background calc(70% + 2px), 
+                        $light_pink 70%
+                    );
+                }
+            }
+            &-container {
+                background-color: $light_pink;
+                grid-column: 1/3;
+                border-radius: 0 20px 20px 20px;
+            }
+        }
+
+        .button {
+            padding: 28px 28px 28px 48px;
+            background-color: $light_background;
+            border-radius: 32px;
+            position: relative;
+            z-index: 1;
+        }
+    }
 }
 </style>
