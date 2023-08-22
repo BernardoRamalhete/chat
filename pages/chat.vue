3bd5f6b0613921e93895f3c66f860ff922c5c74e
BernardoRamalhete <97164408+BernardoRamalhete@users.noreply.github.com>
2023-08-21 21:59:19 -0300

    :construction: Adding chat protected page

---
+++ pages/chat.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <pre>{{ userData }}</pre>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    middleware: ['auth']
+})
+
+const { user } = useFirebaseAuth()
+const userCookie = useCookie('user')
+
+const userData = computed(() => {
+    return user.value || userCookie.value
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
