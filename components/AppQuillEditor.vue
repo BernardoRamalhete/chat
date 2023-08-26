b8d11bcaa7a79323cb1448aff50adc07a2a4e954
BernardoRamalhete <97164408+BernardoRamalhete@users.noreply.github.com>
2023-08-26 14:48:22 -0300

    :sparkles: Create component wrapper for quill editor

---
+++ components/AppQuillEditor.vue
@@ -0,0 +1,46 @@
+<template>
+    <div 
+        id="quill-editor"
+        ref="editorElement"
+        class="editor"
+    />
+</template>
+
+<script setup>
+import 'quill/dist/quill.bubble.css'
+
+const emits = defineEmits(['update:model'])
+
+const editorElement = ref(null)
+
+onMounted(() => {
+    const nuxtApp = useNuxtApp()
+
+    const Quill = nuxtApp.$useQuill()
+
+    const editor = new Quill(editorElement.value, {
+        theme: 'bubble',
+        isolateCharacter: true,
+        modules: {
+            toolbar: [
+                ['bold', 'italic', 'strike'],
+                ['link'],
+                [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+                ['clean']
+            ],
+            magicUrl: {
+                // Regex used to check URLs during typing
+                urlRegularExpression: /(https?:\/\/|www\.)[\w-.]+\.[\w-.]+(\/([\S]+)?)?/gi,
+                // Regex used to check URLs on paste
+                globalRegularExpression: /(https?:\/\/|www\.)[\w-.]+\.[\w-.]+(\/([\S]+)?)?/gi
+            }
+        },
+        placeholder: 'Message...'
+    }).on('text-change', () => {
+        emits('update:model', editorElement.value.querySelector('.ql-editor').innerHTML)
+    })
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
